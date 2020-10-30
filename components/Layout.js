5268aa05dd03710d8e59634a35ba9b274861e137
PKhing <apimrak@gmail.com>
2020-10-31 02:23:15 +0700

    Add admin icon to header

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-export default function Layout({children,username,login}){
+export default function Layout({children,username,login,admin}){
     return(
         <>
         {login?(
@@ -22,11 +22,14 @@ export default function Layout({children,username,login}){
                         username?(
                             <>
                                 <Link href = "/myposts"><a class="inline-block">
-                                    <div class = "-mt-2 rounded-lg bg-white text-xl relative" style={{"height":"50px"}}>
-                                        <div class = "text-white w-8 h-8 text-center absolute" style = {{"backgroundColor":"#AB3B61","border-radius":"50%", "top": "50%","transform": "translate(10px, -50%)"}}>
-                                            <p class="text-2xl font-semibold" style={{"line-height": "30px"}}>{username[0].toUpperCase()}</p>
+                                    <div class = "-mt-2 rounded-lg text-xl relative" style={{"height":"50px","backgroundColor":(admin?"#123D6A":"#FFFFFF")}}>
+                                        <div class = "text-white w-8 h-8 text-center absolute" style = {{"backgroundColor":(admin?"#123D6A":"#AB3B61"),"border-radius":"50%", "top": "50%","transform": "translate(10px, -50%)"}}>
+                                            {admin?(
+                                                <img src = "../images/crown.svg" style={{width:25,transform:"translate(15%,15%)"}}></img>
+                                            ):
+                                            (<p class="text-2xl font-semibold" style={{"line-height": "30px"}}>{username[0].toUpperCase()}</p>)}
                                         </div>
-                                        <p class = "pl-12 pr-2" style={{"fontFamily":"Quark-Bold","fontSize":"18px","transform": "translate(0, 50%)"}}>{username}</p>
+                                        <p class = "pl-12 pr-2" style={{"color":(admin?"#FFFFFF":"#000000"),"fontFamily":"Quark-Bold","fontSize":"18px","transform": "translate(0, 50%)"}}>{username}</p>
                                     </div>
                                 </a></Link>
                             </>
